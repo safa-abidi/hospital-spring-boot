3747e1a84a58596d4516b28e17f6b944830303bd
Hatem Ben Echikh <57599767+hatem782@users.noreply.github.com>
2021-01-21 00:04:20 +0100

    liberer

--- src/main/java/com/example/demo/Controllers/AffecterController.java
+++ src/main/java/com/example/demo/Controllers/AffecterController.java
@@ -2,9 +2,12 @@ package com.example.demo.Controllers;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -64,23 +67,12 @@ public class AffecterController {
 	@RequestMapping("/affecter")
 	public String act2(@RequestParam(name="id_lit") long lit_id , @RequestParam(name="id_Pat") long Pat_id ,Model model) {
 		
-		System.out.println("id_pat :" + Pat_id + " idl_it" + lit_id);
+		System.out.println("id_pat :" + Pat_id + " idl_it" + Pat_id);
 		
-		Patient pf = null;
-		Lit lf = null;
+		Patient pf = patient.getOne(Pat_id);
+		Lit lf = lit_bd.getOne(lit_id);
 		String message= "";
 		
-		for(Patient p : patient.findAll()) {
-			if(p.getCin()== Pat_id) {
-				pf=p;
-			}
-		}
-		
-		for(Lit l : lit_bd.findAll()) {
-			if(l.getLitId()==lit_id) {
-				lf=l;
-			}
-		}
 		
 		if(pf==null || lf==null) {
 			message="Echec d'affectation , réessayer";
@@ -103,4 +95,45 @@ public class AffecterController {
 		return act1(model);
 	}
 
+	@RequestMapping("/Liberer")
+	public String liberer_lit(@RequestParam( name="Id") Long id ) {
+		
+		// changer l'etat du lit
+		Lit l = lit_bd.getOne(id);
+		l.setEtat(false);
+		lit_bd.save(l);
+		
+		List<PatientLit> pat_lit_list = lit_pat_bd.findAll();
+		PatientLit plf = null;
+		
+		// changer les paratemers du patient_lit
+		for( PatientLit pt : pat_lit_list) {
+			if(pt.getLit().getLitId() == id && pt.getPrixTotal()==0) {
+				plf=pt;
+			}
+		}
+		
+		plf.setDateFin(new Date());
+		
+		System.out.println(plf.getDateDebut().getTime());
+		System.out.println(plf.getDateFin().getTime());
+		
+		long a = plf.getDateDebut().getTime();
+		long b = plf.getDateFin().getTime();
+		int number_of_days = (int) ((b-a)/(1000*60*60*24)) ;
+		plf.setPrixTotal(number_of_days * l.getTypeLit().getPrixType());
+		lit_pat_bd.save(plf);
+		
+		
+		// changer l'etat du patient
+		
+		Patient p = plf.getPatient();
+		p.setEtat_patient(false);
+		patient.save(p);
+		
+		
+		System.out.println(id);
+		
+		return "redirect:/lits/liste";
+	}
 }
--- src/main/java/com/example/demo/Controllers/LitController.java
+++ src/main/java/com/example/demo/Controllers/LitController.java
@@ -9,14 +9,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.dao.ChambreRepository;
 import com.example.demo.dao.LitRepository;
+import com.example.demo.dao.TypeRepository;
+import com.example.demo.entities.Chambre;
 import com.example.demo.entities.Lit;
+import com.example.demo.entities.TypeLit;
 
 @Controller
 @RequestMapping(value="lits")
 public class LitController {
 	@Autowired
 	LitRepository lit;
+	@Autowired 
+	ChambreRepository chambres;
+	@Autowired 
+	TypeRepository types;
 	
 	@GetMapping("/liste")
 	public String act1(Model model) {
@@ -28,12 +36,20 @@ public class LitController {
 	@GetMapping("/ajouter")
 	public String act2(Model model) {
 		Lit l = new Lit();
+		
+		List<Chambre> chamb = chambres.findAll();
+		model.addAttribute("list_chambres", chamb);
+		
+		List<TypeLit> type_lit = types.findAll();
+		model.addAttribute("list_types", type_lit);
+		
 		model.addAttribute("lit", l);
-		return "ajouterLit.html";
+		return "ajouterLit";
 	}
 	
 	@PostMapping("/sauvegarder")
 	public String act3(Lit l) {
+		System.out.println(l.getChambre().getDepartement());
 		lit.save(l);
 		return "redirect:liste";
 	}
